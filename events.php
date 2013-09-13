29ab4581e1540dd087f788fdd3141541b0e83b74
Walter Bender <walter@sugarlabs.org>
2013-09-13 09:05:28 -0400

    new project

---
+++ events.php
@@ -0,0 +1,150 @@
+<?php
+include("../include/global.php");
+include("../include/header.php");
+?>
+<div class="contenedor">
+<table style="display: inline-table;" border="0" cellpadding="0" cellspacing="0" width="1000" align="center">
+  <tr>
+   <td colspan="3" style="background-image:url(<?php echo api_get_path(IMG_PATH)?>banner02.jpg); background-repeat:no-repeat; padding-top:20px;vertical-align:top;">
+		<?php
+		include("../include/ingreso.php");
+		?>
+   </td>
+   <td width="1" height="197"></td>
+  </tr>
+  <tr>
+   <td colspan="3" height="33" bgcolor="#FBBF0D">
+	<?php 
+	$modulo="events";
+	include("../include/menu.php");
+	?>
+   </td>
+   <td></td>
+  </tr>
+  <tr>
+   <td colspan="3" style=" border-left:1px; border-left-style:solid; border-left-color:#666; border-right:1px; border-right-style:solid; border-right-color:#666">&nbsp;</td>
+   <td width="1" height="26"></td>
+  </tr>
+  <tr>
+   <td width="58" style=" border-left:1px; border-left-style:solid; border-color: #999;">&nbsp;</td>
+   <td style="vertical-align:top">
+   
+   <table style="display: inline-table;" border="0" cellpadding="0" cellspacing="0" width="876">
+
+
+  <tr>
+   <td colspan="3" width="876" height="28">
+   <label class="titulo">Events</label>
+   </td>
+   <td width="1" height="28"></td>
+  </tr>
+  <tr>
+   <td colspan="3" width="876" height="33">
+   <label class="txt_gen">
+   Sort by: <a class="HiperAma" href="">Date postedsort icon</a> | <a class="HiperAma" href="">Event name</a> | <a class="HiperAma" href="">Event date</a> | <a class="HiperAma" href="">Country</a>
+   </label>
+   </td>
+   <td width="1" height="33"></td>
+  </tr>
+  <tr>
+   <td colspan="3" width="876" height="20"></td>
+   <td width="1" height="20"></td>
+  </tr>
+  <tr>
+   <td width="312" height="156">
+     <img src="../img/Mapa02.jpg" width="311" height="157" /></td>
+   <td width="34" height="156"></td>
+   <td width="530" height="156">
+   <label class="txt_gen">
+<a class="HiperAma">Scratch Day at Bikaner, Rajasthan (June 7, 2013)</a><br />
+<i>Posted 7 weeks 1 hour ago by <a class="HiperAma">Srishti Sethi</a></i> <br /><br /></label>
+
+<table width="300" border="0" class="txt_gen">
+  <tr>
+    <td>Who:</td>
+    <td>New Scratchers</td>
+  </tr>
+  <tr>
+    <td>When:</td>
+    <td>June 7, 2013 - 4:00pm</td>
+  </tr>
+  <tr>
+    <td>Where:</td>
+    <td>Rajasthan, India</td>
+  </tr>
+</table>
+   </td>
+   <td width="1" height="156"></td>
+  </tr>
+  <tr>
+   <td colspan="3" width="876" height="25"></td>
+   <td width="1" height="25"></td>
+  </tr>
+  <tr>
+   <td width="312" height="156">
+     <img src="../img/Mapa03.jpg" width="311" height="157" /></td>
+   <td width="34" height="156"></td>
+   <td width="530" height="156">
+   
+   <label class="txt_gen">
+<a class="HiperAma">Scratch Day in Syros, Greece (June 22, 2013)</a><br />
+<i>Posted 8 weeks 1 day ago by <a class="HiperAma">Katerina Glezou </a></i> <br /><br /></label>
+
+<table width="300" border="0" class="txt_gen">
+  <tr>
+    <td>Who:</td>
+    <td>Teachers</td>
+  </tr>
+  <tr>
+    <td>When:</td>
+    <td>June 22, 2013 - 10:00am</td>
+  </tr>
+  <tr>
+    <td>Where:</td>
+    <td>Greece</td>
+  </tr>
+</table>
+   
+   </td>
+   <td width="1" height="156"></td>
+  </tr>
+  <tr>
+   <td colspan="3" width="876" height="25"></td>
+   <td width="1" height="25"></td>
+  </tr>
+  <tr>
+   <td width="312" height="156">
+     <img src="../img/Mapa04.jpg" width="311" height="157" /></td>
+   <td width="34" height="156"></td>
+   <td width="530" height="156">
+   
+   <label class="txt_gen">
+<a class="HiperAma">3rd Scratch Day Zaragoza 2013)</a><br />
+<i>Posted 8 weeks 5 days ago by <a class="HiperAma">Juan Pradas</a></i> <br /><br /></label>
+
+<table width="300" border="0" class="txt_gen">
+  <tr>
+    <td>Who:</td>
+    <td>Kids</td>
+  </tr>
+  <tr>
+    <td>When:</td>
+    <td>May 25, 2013 - 11:00am</td>
+  </tr>
+  <tr>
+    <td>Where:</td>
+    <td>Spain</td>
+  </tr>
+</table>
+   
+   </td>
+   <td width="1" height="156"></td>
+  </tr>
+  <tr>
+   <td colspan="3" width="876" height="25"></td>
+   <td width="1" height="25"></td>
+  </tr>
+</table>
+<?php
+include("../include/footer.php");
+?>
